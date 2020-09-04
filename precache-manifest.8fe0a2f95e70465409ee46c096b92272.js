546264fc5ba5afe4933bf8a114297120550a8a36
Manduul Enkhee <manduu.enkhe@gmail.com>
2020-09-03 21:14:41 -0700

    Updates

---
+++ precache-manifest.8fe0a2f95e70465409ee46c096b92272.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "044df7be985aa36bf5fe7fc034395167",
+    "url": "/Projects/index.html"
+  },
+  {
+    "revision": "41a237ca3841fe34faea",
+    "url": "/Projects/static/css/main.cb6fb1fb.chunk.css"
+  },
+  {
+    "revision": "a085a67b0c1c8d1e26df",
+    "url": "/Projects/static/js/2.dfefe7a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Projects/static/js/2.dfefe7a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a237ca3841fe34faea",
+    "url": "/Projects/static/js/main.93e165e0.chunk.js"
+  },
+  {
+    "revision": "fecc15e8ec3be6e90416",
+    "url": "/Projects/static/js/runtime-main.a036bc69.js"
+  },
+  {
+    "revision": "8301b90a568a0341dfa2544098f7b840",
+    "url": "/Projects/static/media/3.8301b90a.jpg"
+  }
+]);
